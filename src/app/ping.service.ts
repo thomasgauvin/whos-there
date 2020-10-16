f30efa2d9c810c0657eb149ccfdbe6fcdce30314
Kevin Barnes <kbarnes3@gmail.com>
2020-10-16 16:10:50 -0700

    Data is returned from a service

--- src/app/ping.service.ts
+++ src/app/ping.service.ts
@@ -1,20 +1,36 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject, Observable, of, Subscription, timer } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PingService {
   private pageViewersSubject: BehaviorSubject<string[]>;
-
   pageViewers$: Observable<string[]>;
 
-  constructor() {
+  private pingUrl: string = '/api/ping';
+
+  private subscription: Subscription;
+
+  constructor(private http: HttpClient) {
     this.pageViewersSubject = new BehaviorSubject<string[]>(['Kevin']);
     this.pageViewers$ = this.pageViewersSubject.asObservable();
+
+    this.subscription = timer(0, 5000).subscribe(result => {
+      this.updatePageViewers();
+    });
    }
 
    get pageViewers(): string[] {
      return this.pageViewersSubject.getValue();
+   }
+
+   private updatePageViewers(): void {
+    this.http.get(this.pingUrl).pipe(catchError(error => of([])))
+      .subscribe((pageViewers: string[]) => {
+        this.pageViewersSubject.next(pageViewers);
+      });
    }
 }
